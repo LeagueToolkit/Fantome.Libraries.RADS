c7386a5ed204bf7622f09d217c182a8e691af12e
Chewy <chewybomber@gmail.com>
2018-07-28 14:51:00 +0200

    Added release manifest + RAF code

--- Fantome.Libraries.RADS.Tests/Program.cs
+++ Fantome.Libraries.RADS.Tests/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Fantome.Libraries.RADS.IO.ReleaseManifest;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,18 @@ namespace Fantome.Libraries.RADS.Tests
     {
         static void Main(string[] args)
         {
+            ReleaseManifestTest();
+        }
+
+        static void ReleaseManifestTest()
+        {
+            ReleaseManifestFile relMan = new ReleaseManifestFile(new MemoryStream(new System.Net.WebClient().DownloadData("http://l3cdn.riotgames.com/releases/live/projects/lol_game_client/releases/0.0.0.0/releasemanifest")));
+            relMan.Project.Remove();
+            relMan.Write("myrealm");
+            using (FileStream fs = File.Open("myrealm2", FileMode.Create))
+            {
+                relMan.Write(fs);
+            }
         }
     }
 }
