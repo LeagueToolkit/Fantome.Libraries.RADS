9156dcfca39278501e733a042fe52525f5d43e96
Chewy <chewybomber@gmail.com>
2018-08-05 17:42:47 +0200

    Solution manifest readers moved to RADS lib

---
+++ Fantome.Libraries.RADS/IO/SolutionManifest/SolutionManifestLocalizedEntry.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Fantome.Libraries.RADS.IO.SolutionManifest
+{
+    public class SolutionManifestLocalizedEntry
+    {
+        public string Name { get; private set; }
+        public int Unknown { get; private set; }
+        public List<SolutionManifestProjectEntry> Projects { get; private set; }
+
+        public SolutionManifestLocalizedEntry(string name, int unknown, List<SolutionManifestProjectEntry> projects)
+        {
+            this.Name = name;
+            this.Unknown = unknown;
+            this.Projects = projects;
+        }
+    }
+}
